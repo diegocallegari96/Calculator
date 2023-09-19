286d28d88fafaaa0ad23545c52cbc4758320cba7
diegocallegari96 <warsindustry@gmail.com>
2023-09-19 19:30:26 +0200

    button functionality

--- App.jsx
+++ App.jsx
@@ -1,13 +1,39 @@
 import './App.css'
+import Button from "./components/Button/Button.jsx";
+import ButtonBox from "./components/ButtonBox/ButtonBox.jsx";
+import Screen from "./components/Screen/Screen.jsx";
+import Wrapper from "./components/Wrapper/Wrapper.jsx";
 
-function App() {
+const btnValues = [
+    ["C", "+-", "%", "/"],
+    [7, 8, 9, "X"],
+    [4, 5, 6, "-"],
+    [1, 2, 3, "+"],
+    [0, ".", "="],
+];
 
-
+const App = () => {
   return (
-    <>
+      <Wrapper>
+        <Screen value="0" />
+        <ButtonBox>
+          {
+            btnValues.flat().map((btn, i) => {
+              return (
+                  <Button
+                    key={i}
+                    className={btn === "=" ? "equals" : ""}
+                    value={btn}
+                    onClick={() => {
+                      console.log(`${btn} clicked!`);
+                    }}
+                  />
+              );
+            })
+          }
+        </ButtonBox>
+      </Wrapper>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
